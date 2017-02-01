6af9f050a7e58368ff4f60ecbea5bc6db4ce1a38
Takaya Suzuki <tk.yaaa7@gmail.com>
2017-02-01 11:46:02 +0700

    pt2

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,5 +1,11 @@
 <?php
+
 function def_path(){
 	$url = 'http://suisen-book.com/';
+	return (string) $url;
+}
+
+function cus_path($str){
+	$url = 'http:\\suisen-book.com\\' . $str;
 	return (string) $url;
 }
