d41d086f4447147f280f16a9f61c43aae4cd6b10
Takaya Suzuki <tk.yaaa7@gmail.com>
2017-02-02 23:53:13 +0700

    pt2

--- resources/views/layouts/user/books.php
+++ resources/views/layouts/user/books.php
@@ -23,55 +23,6 @@
 <div id="dammy"></div>
 
 <main>
-<div id="form">
-	<h3>マイページ</h3>
-
-{!! Form::open(['url' => ['my-page/update', Auth::user()->id], 'files' => true]) !!}
-<table>
-<tbody>
-	<tr>
-		<th>Image</th>
-		<td>
-			{!! Form::file('avatar')!!}
-		</td>
-	</tr>
-	<tr>
-		<th>User Name</th>
-		<td>
-			{!! Form::text('name', isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email) !!}
-		</td>
-	</tr>
-	<tr>
-		<th>Email</th>
-		<td>
-			{!! Form::email('email', Auth::user()->email ) !!}
-		</td>
-	</tr>
-	<tr>
-		<th>Password</th>
-		<td>
-			{!! Form::password('password') !!}
-		</td>
-	</tr>
-	<tr>
-		<th>New Password</th>
-		<td>
-			{!! Form::password('passwordNew') !!}
-		</td>
-	</tr>
-	<tr>
-		<th>New Password （確認）</th>
-		<td>
-			{!! Form::password('passwordNewConfirm') !!}
-		</td>
-	</tr>
-</tbody>
-</table>
-	<p>
-		{!! Form::submit('Save', ['id' => 'formbtn']) !!}
-	</p>
-{!! Form::close() !!}
-</div><!-- #form -->
 </main>
 
 @endsection
