192e5fef01b464f2702c5480705b768a318bc13a
Takaya Suzuki <tk.yaaa7@gmail.com>
2017-02-04 01:58:15 +0700

    pt2

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -9,3 +9,12 @@ function cus_path($str){
 	$url = 'http:\\suisen-book.com\\' . $str;
 	return (string) $url;
 }
+
+function get_env(){
+        if (App::environment('local')) {
+            $env = "http://localhost:81/pt2/advertize_design";
+        } else if(App::environment('production')) {
+            $env = "http://suisen-book.com/advertize_design";
+        }
+        return $env;
+}
--- resources/views/advertize/index.blade.php
+++ resources/views/advertize/index.blade.php
@@ -129,7 +129,7 @@
 						<div class="row">
 							<div class="col-md-12 text-center">
 								<h1 class="wp1">Do you want to visit our real page?? (Not kidding) Click here.</h1>
-								<a href="http://suisen-book.com" class="btn danger wp2">No kidding page <br /> (Only Japanese)</a>
+								<a href="http://suisen-book.com" class="btn primary wp2">No kidding page <br /> (Only Japanese)</a>
 							</div>
 						</div>
 						<div class="row">
