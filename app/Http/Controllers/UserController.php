cbce92ea13b0d0e468f963da5b4dc0a26775a7c6
Takaya Suzuki <tk.yaaa7@gmail.com>
2017-02-06 00:00:20 +0700

    pt2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -121,8 +121,8 @@ class UserController extends Controller
 
     public function myBooks()
     {
-        $books = DB::table('books')->select('id', 'title')->where('user_id', Auth()->id())->get()->paginate(5);
-        // $books = Book::get(['id', 'title'])->where('user_id', Auth()->id());
+        // $books = DB::table('books')->select('id', 'title')->where('user_id', Auth()->id())->get()->paginate(5);
+        $books = Book::get()->where('user_id', Auth()->id())->paginate(5);
         return view('layouts.user.books', compact('books'));
     }
 
